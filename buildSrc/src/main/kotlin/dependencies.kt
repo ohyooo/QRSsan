9d84907cf75d518c7cb687b555969e00b3f10075
ohyooo <runner@fv-az449-204.jvjgdiodbn4ungrcbaap0pmvbb.dx.internal.cloudapp.net>
2022-03-16 23:46:13 +0000

    update libs

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -25,7 +25,7 @@ object Libs {
         const val material = "com.google.android.material:material:1.6.0-alpha03"
         const val barcode = "com.google.mlkit:barcode-scanning:17.0.2"
 
-        private const val accompanistVersion = "0.24.3-alpha"
+        private const val accompanistVersion = "0.24.4-alpha"
         const val pager = "com.google.accompanist:accompanist-pager:$accompanistVersion"
         const val indicators = "com.google.accompanist:accompanist-pager-indicators:$accompanistVersion"
     }
