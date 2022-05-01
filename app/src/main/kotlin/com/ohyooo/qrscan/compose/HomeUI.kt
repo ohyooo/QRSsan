2839db304569037ca3ffe84e742f7d308e242f42
ohyooo <i@ohyooo.com>
2022-05-01 12:21:25 +0800

    update

--- app/src/main/kotlin/com/ohyooo/qrscan/compose/HomeUI.kt
+++ app/src/main/kotlin/com/ohyooo/qrscan/compose/HomeUI.kt
@@ -3,15 +3,7 @@ package com.ohyooo.qrscan.compose
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.BottomSheetScaffold
-import androidx.compose.material.BottomSheetState
-import androidx.compose.material.BottomSheetValue
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.Tab
-import androidx.compose.material.TabRow
-import androidx.compose.material.TabRowDefaults
-import androidx.compose.material.Text
-import androidx.compose.material.rememberBottomSheetScaffoldState
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
@@ -19,11 +11,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.google.accompanist.pager.ExperimentalPagerApi
-import com.google.accompanist.pager.HorizontalPager
-import com.google.accompanist.pager.PagerState
-import com.google.accompanist.pager.pagerTabIndicatorOffset
-import com.google.accompanist.pager.rememberPagerState
+import com.google.accompanist.pager.*
 import com.ohyooo.qrscan.ScanActivity
 import kotlinx.coroutines.launch
 
@@ -47,7 +35,7 @@ fun MainUI(activity: ScanActivity) {
         sheetContent = {
             Home(activity = activity)
         },
-        sheetPeekHeight = 80.dp,
+        sheetPeekHeight = 100.dp,
         modifier = Modifier.fillMaxSize(),
     ) {
         CameraUI(activity)
