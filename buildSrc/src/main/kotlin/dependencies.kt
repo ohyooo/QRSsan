7cb0bc1b6d632b603fb2dc8723ba6d10d413ee63
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2022-06-07 17:12:37 +0800

    Update dependencies.kt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -9,7 +9,7 @@ object Ext {
 }
 
 object Libs {
-    private const val kotlin_version = "1.6.21"
+    private const val kotlin_version = "7.4.0-alpha03"
 
     object Plugin {
         const val AGP = "com.android.tools.build:gradle:7.2.1"
