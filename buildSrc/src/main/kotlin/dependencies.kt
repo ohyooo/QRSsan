86f522f2fa09a1e18300b8d693eddc17efe82d56
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2022-07-28 14:12:36 +0800

    Update dependencies.kt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -45,8 +45,8 @@ object Libs {
     }
 
     object Compose {
-        const val composeVersion = "1.3.0-alpha01"
-        const val compilerVersion = "1.3.0-beta01"
+        const val composeVersion = "1.3.0-alpha02"
+        const val compilerVersion = "1.3.0-rc01"
         const val compiler = "androidx.compose.compiler:compiler:$compilerVersion"
         const val foundation = "androidx.compose.foundation:foundation:$composeVersion"
         const val layout = "androidx.compose.foundation:foundation-layout:$composeVersion"
