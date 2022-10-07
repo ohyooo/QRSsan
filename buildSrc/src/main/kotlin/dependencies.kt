7117fa5e2c7f20adc6a7372f0858be12ca79c961
ba la~ ba la~ <8428270+ohyooo@users.noreply.github.com>
2022-10-07 21:28:31 +0800

    Update dependencies.kt

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -31,17 +31,17 @@ object Libs {
     }
 
     object AndroidX {
-        const val coreKtx = "androidx.core:core-ktx:1.8.0"
+        const val coreKtx = "androidx.core:core-ktx:1.9.0"
         const val datastore = "androidx.datastore:datastore-preferences:1.0.0"
-        const val activity_compose = "androidx.activity:activity-compose:1.5.1"
-        const val fragment = "androidx.fragment:fragment-ktx:1.5.2"
-        const val lifecycle = "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1"
+        const val activity_compose = "androidx.activity:activity-compose:1.6.0"
+        const val fragment = "androidx.fragment:fragment-ktx:1.5.3"
+        const val lifecycle = "androidx.lifecycle:lifecycle-viewmodel-compose:2.5.2"
         const val serialization = "org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0"
         const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4"
     }
 
     object Navigation {
-        const val compose = "androidx.navigation:navigation-compose:2.5.1"
+        const val compose = "androidx.navigation:navigation-compose:2.5.2"
     }
 
     object Compose {
