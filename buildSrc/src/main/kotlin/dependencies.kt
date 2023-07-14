91e17468167880f99d292c73f9854e18854589ea
i <i@ohyooo.com>
2023-07-14 10:48:48 +0800

    v2.10b10

--- buildSrc/src/main/kotlin/dependencies.kt
+++ buildSrc/src/main/kotlin/dependencies.kt
@@ -3,8 +3,8 @@ object Ext {
     const val compileSdk = 34
     const val minSdk = 21
     const val targetSdk = 34
-    const val versionCode = 9
-    const val versionName = "2.9"
+    const val versionCode = 10
+    const val versionName = "2.10"
 }
 
 object Libs {
